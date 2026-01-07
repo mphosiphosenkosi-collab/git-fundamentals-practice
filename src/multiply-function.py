c9d1fa72681fc37caf01163bee04a07513a25271
Siphosenkosi <Siphosenkosi17@outlook.com>
2026-01-07 14:09:43 +0200

    Chore: adding the multiply function in our f2

---
+++ src/multiply-function.py
@@ -0,0 +1,6 @@
+def multiply(a, b):
+    # Multiply two numbers
+    result = a * b
+    print(f'Multiplying {a} x {b}')
+    return result
+print(f'Multiply function loaded: {multiply(3, 4)}')  # Test the function
